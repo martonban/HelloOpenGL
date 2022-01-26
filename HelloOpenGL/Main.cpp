cd788a2f00f0ede4af80ce874be6536091a28e27
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-01-26 22:31:03 +0100

    Shader

--- HelloOpenGL/Main.cpp
+++ HelloOpenGL/Main.cpp
@@ -10,12 +10,12 @@
 
 GLfloat vertices[] ={
 
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
+	-0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f,
+	0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f,
+	0.0f,		0.5f * float(sqrt(3)) * 2 / 3,	0.0f,		1.0f, 0.60f, 0.32f,
+	-0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
+	0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
+	0.0f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f
 };
 
 
@@ -57,7 +57,8 @@ int main(){
 
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAtrib(VBO1, 0, 3, GL_FLOAT, 6*sizeof(float), (void*)0);
+	VAO1.LinkAtrib(VBO1, 1, 3, GL_FLOAT, 6*sizeof(float), (void*)(3* sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
