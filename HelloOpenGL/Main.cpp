3206bd103759563c3fbbd3e8763e68be8338485f
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-03-26 20:31:27 +0100

    Texture

--- HelloOpenGL/Main.cpp
+++ HelloOpenGL/Main.cpp
@@ -6,25 +6,21 @@
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
-#include<stb/stb_image.h> 
+#include<stb/stb_image.h>
 
 
-GLfloat vertices[] ={
-
-	-0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f,
-	0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f,
-	0.0f,		0.5f * float(sqrt(3)) * 2 / 3,	0.0f,		1.0f, 0.60f, 0.32f,
-	-0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
-	0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
-	0.0f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.30f, 0.02f
+GLfloat vertices[] ={ 
+	//     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
 };
 
 
 GLuint indices[] ={
-
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1,
+	0, 3, 2
 };
 
 
@@ -58,16 +54,48 @@ int main(){
 
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
-	VAO1.LinkAtrib(VBO1, 0, 3, GL_FLOAT, 6*sizeof(float), (void*)0);
-	VAO1.LinkAtrib(VBO1, 1, 3, GL_FLOAT, 6*sizeof(float), (void*)(3* sizeof(float)));
+	VAO1.LinkAtrib(VBO1, 0, 3, GL_FLOAT, 8*sizeof(float), (void*)0);
+	VAO1.LinkAtrib(VBO1, 1, 3, GL_FLOAT, 8*sizeof(float), (void*)(3* sizeof(float)));
+	VAO1.LinkAtrib(VBO1, 2, 2, GL_FLOAT, 8* sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	
 
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("texture.png", &widthImg, &heightImg, &numColCh, 0);
+
+	if (bytes) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	std::cout << "Failed to load texture" << std::endl;
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1f(tex0Uni, 0);
 
 	while (!glfwWindowShouldClose(window)){
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -77,10 +105,10 @@ int main(){
 
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
-
+		glBindTexture(GL_TEXTURE_2D, texture);
 		VAO1.Bind();
 
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
@@ -91,6 +119,7 @@ int main(){
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
