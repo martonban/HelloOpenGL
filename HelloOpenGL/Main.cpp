c08f0e62698616b23aaca6f4665d76e9ad029509
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-07-25 20:01:37 +0200

    math lib include

--- HelloOpenGL/Main.cpp
+++ HelloOpenGL/Main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
 #include "Texture.h"
-#include <stb/stb_image.h>
+
 
 GLfloat vertices[] = {
 	//     COORDINATES     /        COLORS      /   TexCoord  //
