30f8a8f91432f239610f6dae603f8f3abba645fb
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-07-25 16:31:40 +0200

    Texture Class

--- HelloOpenGL/Main.cpp
+++ HelloOpenGL/Main.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-#include"shaderClass.h"
-#include"VAO.h"
-#include"VBO.h"
-#include"EBO.h"
-#include<stb/stb_image.h>
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+#include "Texture.h"
+#include <stb/stb_image.h>
 
-
-GLfloat vertices[] ={ 
+GLfloat vertices[] = {
 	//     COORDINATES     /        COLORS      /   TexCoord  //
 	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
 	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
@@ -18,14 +18,14 @@ GLfloat vertices[] ={
 };
 
 
-GLuint indices[] ={
+GLuint indices[] = {
 	0, 2, 1,
 	0, 3, 2
 };
 
 
 
-int main(){
+int main() {
 
 	glfwInit();
 
@@ -36,7 +36,7 @@ int main(){
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Hello OpenGL", NULL, NULL);
 
-	if (window == NULL){
+	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -54,58 +54,32 @@ int main(){
 
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
-	VAO1.LinkAtrib(VBO1, 0, 3, GL_FLOAT, 8*sizeof(float), (void*)0);
-	VAO1.LinkAtrib(VBO1, 1, 3, GL_FLOAT, 8*sizeof(float), (void*)(3* sizeof(float)));
-	VAO1.LinkAtrib(VBO1, 2, 2, GL_FLOAT, 8* sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAtrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAtrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAtrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+
+
+	
+	//Texture
+	Texture wallTexture("texture.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	wallTexture.texUnit(shaderProgram, "tex0", 0);
+
+
 	
 
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int widthImg, heightImg, numColCh;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* bytes = stbi_load("texture.png", &widthImg, &heightImg, &numColCh, 0);
-
-	if (bytes) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-
-	std::cout << "Failed to load texture" << std::endl;
-
-	stbi_image_free(bytes);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
-	shaderProgram.Activate();
-	glUniform1f(tex0Uni, 0);
-
-	while (!glfwWindowShouldClose(window)){
+	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
 		glClear(GL_COLOR_BUFFER_BIT);
-
-
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
-		glBindTexture(GL_TEXTURE_2D, texture);
+		wallTexture.Bind();
+
 		VAO1.Bind();
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -119,7 +93,7 @@ int main(){
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
-	glDeleteTextures(1, &texture);
+	wallTexture.Delete();
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
