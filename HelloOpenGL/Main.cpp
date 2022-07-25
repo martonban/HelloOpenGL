db2529206502ed96aec1bd7c786fae161a9f1022
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-07-26 00:30:45 +0200

    3D Pyramid DONE

--- HelloOpenGL/Main.cpp
+++ HelloOpenGL/Main.cpp
@@ -18,17 +18,22 @@ const unsigned int height = 800;
 
 
 GLfloat vertices[] = {
-	//     COORDINATES     /        COLORS      /   TexCoord  //
-	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
-	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+	//     COORDINATES     /        COLORS      /   TexCoord 
+	-0.5f,	0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f, 
+	-0.5f,  0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f, 
+	 0.5f,  0.0f, -0.5f,     0.83f, 0.70f, 1.44f,	0.0f, 0.0f, 
+	 0.5f,	0.0f,  0.5f,     0.83f, 0.70f, 1.44f,	5.0f, 0.0f, 
+	 0.0f,	0.8f,  0.0f,     0.92f, 0.86f, 1.76f,	2.5f, 5.0f  
 };
 
 
 GLuint indices[] = {
-	0, 2, 1,
-	0, 3, 2
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 
@@ -78,19 +83,30 @@ int main() {
 	Texture wallTexture("texture.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	wallTexture.texUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
 
 	
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 60) {
+			rotation += 0.03f;
+			prevTime = crntTime;
+		}
 
 		//Mátrix inicializálás
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
 
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
 		//Frustum
 		proj = glm::perspective(glm::radians(30.0f), (float)(width / height), 0.1f, 100.0f);
@@ -105,7 +121,7 @@ int main() {
 		glUniform1f(uniID, 0.5f);
 		wallTexture.Bind();
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
